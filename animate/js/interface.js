466e6d9e1c2af404d40e70d4ea0171bfe490be21
owenroberts <obr208@nyu.edu>
2018-02-02 16:13:36 -0500

    working on frame uis

--- animate/js/interface.js
+++ animate/js/interface.js
@@ -4,9 +4,61 @@ function Interface() {
 	this.panels = {};
 	this.interfaces = {}; /* better name for interfaces ?? */
 
-	this.framesPanel = new UI({id:"frames"});
+	this.framesPanel = new Panel("frames");
 	this.plusFrame = new UI({id:"current"}); /* plus frame is unsaved drawing frame */
 	this.frameElems = new UIList({class:"frame"});
+
+	this.makeFrame = function(n) {
+		const frm = new UIFrame({
+			event: "click",
+			title: n,
+			index: n,
+			callback: function(ev) {
+				Lines.currentFrame = Lines.currentFrameCount = this.dataset.index;
+				self.updateFrameNum();
+			}
+		});
+		self.framesPanel.add(frm); // this is the right place for this to happen not in ui.js
+
+		// how to make this part of UI?
+		/* right click, add/remove from copy frames */
+		frm.el.oncontextmenu = function(ev) {
+			ev.preventDefault();
+			let i = this.dataset.index;
+			if (!this.classList.toggle("copy")){
+				Lines.data.framesToCopy.splice(Lines.data.framesToCopy.indexOf(i), 1);
+			} else {
+				this.classList.add("copy");
+				Lines.data.framesToCopy.push(i);
+			}
+		};
+
+		const expand = document.createElement('div');
+		expand.textContent = ">";
+		frm.append(expand);
+		expand.onclick = function() {
+			if (this.textContent == ">") {
+				this.textContent = "<";
+				this.parentNode.style.width = "100px";
+			} else {
+				this.textContent = ">";
+				this.parentNode.style.width = "auto";
+			}
+		}
+
+		/* this is probably only happening here ... */
+		/* do this w UI List? */
+		//this.framesPanel.el.insertBefore(frm.el, self.plusFrame.el);
+
+		/* 
+		add drawing nums, do this later after figuring out lines/frames/drawings 
+		frmElem.innerHTML = i + "<br>";
+		for (let d = 0; d < frames[i].length; d++) {
+			frmElem.innerHTML += frames[i][d].d;
+		}
+		*/
+		return frm;
+	}
 
 	/* updates the frame panel representation of frames, sets current frame, sets copy frames */
 	this.updateFramesPanel = function() {
@@ -15,41 +67,15 @@ function Interface() {
 			loop from the num of already made html frames to frames.length */
 		if (Lines.frames.length > numFrames) {
 			for (let i = numFrames; i < Lines.frames.length; i++) {
-				const frmElem = document.createElement("div");
-				frmElem.classList.add("frame");
-				frmElem.textContent = i;
-				frmElem.dataset.index = i;
-				/* 
-				add drawing nums, do this later after figuring out lines/frames/drawings 
-				frmElem.innerHTML = i + "<br>";
-				for (let d = 0; d < frames[i].length; d++) {
-					frmElem.innerHTML += frames[i][d].d;
-				}
-				*/
-				/* click on frame, set the current frame */
-				frmElem.onclick = function(ev) {
-					Lines.currentFrame = Lines.draw.currentFrameCounter = i;
-					self.updateFrameNum();
-				};
-				/* right click, add/remove from copy frames */
-				frmElem.oncontextmenu = function(ev) {
-					ev.preventDefault();
-					if (!this.classList.toggle("copy")){
-						Lines.data.framesToCopy.splice(Lines.data.framesToCopy.indexOf(i), 1);
-					} else {
-						this.classList.add("copy");
-						Lines.data.framesToCopy.push(i);
-					}
-				};
-				/* this is probably only happening here ... */
-				this.framesPanel.el.insertBefore(frmElem, self.plusFrame.el);
-				/* not updating length of frame elems, shouldnt that reference still work?  */
+				const f = self.makeFrame(i);
+				self.frameElems.addUI(f);
 			}
 		} else {
 			/* if there are same number of less then frames than frame divs
 				delete current frame, is there another way to delete frames?  */
 			for (let i = numFrames; i > Lines.frames.length; i--){
 				/* remove html frame */
+				this.frameElems
 				this.frameElems.remove(i-1);
 			}
 		}
@@ -60,7 +86,7 @@ function Interface() {
 		Lines.draw.frameNumDisplay.set(Lines.currentFrame);
 		if (document.getElementById("current"))
 			document.getElementById("current").removeAttribute("id"); /* fine for now... */
-		if (self.frameElems.els[Lines.currentFrame])
+		if (self.frameElems.uis[Lines.currentFrame])
 			self.frameElems.setId("current", Lines.currentFrame);
 		else 
 			self.plusFrame.setId("current");
